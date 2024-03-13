605dc2d36ffea05d460441f1ee3a4f5679eb562a
Arthur <arthur.barrabonita@gmail.com>
2024-03-13 12:23:14 -0300

    copiar

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 import ImageViewer from './src/components/ImageViewer';
 import Button from './src/components/Button';
 import * as ImagePicker from 'expo-image-picker';
 
-const PlaceholderImage = require('./assets/pelé.jpg');
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled){
+      selectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("Você não selecionou a imagem");
+    }
+  };
+  
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+        <ImageViewer 
+        placeholderImageSource={PlaceholderImage}
+        selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Escolher Foto" theme="primary"/>
-        <Button label="Usar esta Foto"/>
+        <Button 
+          label="Escolher Foto" 
+          theme="primary"
+          onPress={pickImageAsync}
+          />
+        <Button 
+        label="Usar esta Foto"
+        onPress={() => setShowAppOptions(true)}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
